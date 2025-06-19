4b488b53506fccb0c972b413d48f27b11473f0d7
Dulankw789 <212723397+Dulankw789@users.noreply.github.com>
2025-06-19 11:13:49 +0300

    Update isOwner.js
    Signed-off-by: Dulankw789 <212723397+Dulankw789@users.noreply.github.com>

--- isOwner.js
+++ isOwner.js
@@ -2,8 +2,8 @@ const settings = require('../settings');
 
 function isOwner(senderId) {
     // Get owner number from settings
-    const ownerJid = settings.ownerNumber + "@s.whatsapp.net";
+    const ownerJid = settings.ownerNumber +94 771396524 "@s.whatsapp.net";
     return senderId === ownerJid;
 }
 
-module.exports = isOwner;
+module.exports = isOwner;sanka
