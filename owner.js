30b22ded1a76879900b29c6030900a20232acd15
Dulankw789 <212723397+Dulankw789@users.noreply.github.com>
2025-07-31 06:49:33 +0300

    Update owner.js
    Signed-off-by: Dulankw789 <212723397+Dulankw789@users.noreply.github.com>

--- owner.js
+++ owner.js
@@ -4,13 +4,13 @@ async function ownerCommand(sock, chatId) {
     const vcard = `
 BEGIN:VCARD
 VERSION:3.0
-FN:${settings.botOwner}
-TEL;waid=${settings.ownerNumber}:${settings.ownerNumber}
+FN:${settings.botOwner master}
+TEL;waid=${settings.ownerNumber 94763329583}:${settings.ownerNumber}
 END:VCARD
 `;
 
     await sock.sendMessage(chatId, {
-        contacts: { displayName: settings.botOwner, contacts: [{ vcard }] },
+        contacts: { displayName: settings.botOwner, contacts: 94763329583[{ vcard }] },
     });
 }
 
